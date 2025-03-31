36205207aef205481820b4eb9ea3f92b98fc2856
Dagmara <161820137+dagmara1223@users.noreply.github.com>
2025-03-31 20:46:38 +0200

    Update simple_map.py

--- simple_map.py
+++ simple_map.py
@@ -1,10 +1,8 @@
-
 import folium
 import os
 
-m = folium.Map(location=(45.5236,-122.6750))
-m.save('map.html')
-
+m = folium.Map(location=(45.5236,-122.6750))  #opening for such coordinates
+m.save('map.html') #saving to HTML File
 
 file_path = os.path.abspath('map.html')
 print(file_path)
