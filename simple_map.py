f501470e80899c883d97e1f8f1364cce9b1e2c47
danielsterzel <166725057+danielsterzel@users.noreply.github.com>
2025-03-31 21:29:51 +0200

    Update simple_map.py

--- simple_map.py
+++ simple_map.py
@@ -1,10 +1,14 @@
-
 import folium
-import os
 
-m = folium.Map(location=(45.5236,-122.6750))
+
+m = folium.Map(location=[45.35, -121.6972], zoom_start=12)
+
+m.add_child(folium.CircleMarker(location=(45.45, -121.6950)))
+
+folium.Marker(
+    location=(45.3488,-121.6932),
+    tooltip='click me!',
+    icon=folium.Icon(icon='fa-solid fa-car-side', color='red',prefix='fa')
+).add_to(m)
+
 m.save('map.html')
-
-
-file_path = os.path.abspath('map.html')
-print(file_path)
